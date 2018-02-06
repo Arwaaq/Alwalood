c87e65774eae47efe002270874ca8745556b7d60
Mashael Gm <mashaelgm@MacBook-Air-alkhas-b-Mashael.local>
2018-02-07 00:54:09 +0300

    Initial Commit

---
+++ Alwalood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alwalood
+//
+//  Created by Mashael Gm on 2/7/18.
+//  Copyright © 2018 Mashael Gm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
